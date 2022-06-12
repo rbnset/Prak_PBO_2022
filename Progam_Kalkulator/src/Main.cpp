ff91f004fdf1a718fceabbdfabe23439343c02b6
rbnsetiyawan <rbn.setiyawan@gmail.com>
2022-06-13 05:39:26 +0700

    Program kalkulator sederhana

--- Progam_Kalkulator/src/Main.cpp
+++ Progam_Kalkulator/src/Main.cpp
@@ -4,8 +4,44 @@ using namespace std;
 
 int main()
 {
-    float a, b;
-    cout <<"Selamat datang di program calculator \n \n";
+    float a, b, hasil;
+    char aritmatika;
+
+    cout << "Selamat datang di program calculator \n \n";
+
+    // Masukkan input dari user
+    cout << "Masukkan nilai pertama : ";
+    cin >> a;
+    cout << "Pilih operator +,-,/,* : ";
+    cin >> aritmatika;
+    cout << "Masukkan nilai kedua : ";
+    cin >> b;
+
+    cout << "\nHasil perhitungan : ";
+    cout << a << aritmatika << b << endl;
+
+    if (aritmatika == '+')
+    {
+        hasil = a + b;
+    }
+    else if (aritmatika == '-')
+    {
+        hasil = a - b;
+    }
+    else if (aritmatika == '/')
+    {
+        hasil = a / b;
+    }
+    else if (aritmatika == '*')
+    {
+        hasil = a * b;
+    }
+    else
+    {
+        cout << "operator yang anda masukkan salah" << endl;
+    }
+
+    cout << " = " << hasil << endl;
 
     cin.get();
     return 0;
